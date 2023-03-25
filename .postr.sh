aa3e50eed8ff3e8e16789ebedb054bc86500358c
MrHacker X <70113933+MrHacker-X@users.noreply.github.com>
2023-03-26 01:22:04 +0530

    Create .postr.sh

---
+++ .postr.sh
@@ -0,0 +1,36 @@
+clear
+cat .skl
+sleep $1
+clear
+sleep $1
+cat .skl
+sleep $1
+clear
+sleep $1
+cat .skl
+sleep $1
+clear
+sleep $1
+cat .skl
+sleep $1
+clear
+sleep $1
+cat .skl
+sleep $1
+clear
+sleep $1
+cat .skl
+sleep $1
+clear
+sleep $1
+cat .skl
+sleep $1
+clear
+sleep $1
+cat .skl
+sleep $1
+clear
+sleep $1
+cat .skl
+sleep $1
+clear
